b59649530919507b155fb543fca28f020a9953f5
kaciojava <kaciosilvanovo@gmail.com>
2025-06-18 22:58:26 -0300

    .

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const startButton = Document.getElementbyid(`startButton`)
-document.getElementById('startButton').addEventListener('click', function() {
+document.getElementById('startButton').addEventListener('click', function () {
   alert('Iniciando...')
 }
+
 function iniciarjogo() {
 
-    
-}
+  }
 function reset() {
-    
-}
+
+  }
 function creditos() {
-    
-}
+
+  }
